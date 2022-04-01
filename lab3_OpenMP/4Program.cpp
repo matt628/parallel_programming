1cc4d04d96dbd52071b90fd91902410ccae76cea
Mateusz Obrzut <mobrzut@d17-430-pc-12.ipa.ki.agh.edu.pl>
2022-04-01 17:17:27 +0200

    lab3

--- lab3_OpenMP/4Program.cpp
+++ lab3_OpenMP/4Program.cpp
@@ -3,9 +3,11 @@
 
 int main () {
     int v1, v2, v3;
+    
 
-    printf("Liczba wątków: %d\n", omp_get_num_threads());
-    printf("Jestem wątek nr: %d\n", omp_get_thread_num());
-
+    # pragma omp parallel {
+        printf("Liczba wątków: %d\n", omp_get_num_threads());
+        printf("Jestem wątek nr: %d\n", omp_get_thread_num());
+    }
     return 0;
 }
