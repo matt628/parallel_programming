c7c7333e4d64c6455dac645051b1893995419325
Mateusz Obrzut <mobrzut@d17-430-pc-12.ipa.ki.agh.edu.pl>
2022-04-01 18:04:06 +0200

    a

--- lab3_OpenMP/5Program.c
+++ lab3_OpenMP/5Program.c
@@ -8,12 +8,10 @@ int main () {
     srand(time(NULL));   // Initialization, should only be called once.
     // int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
     int arr[MAX_SIZE+10];
-    omp_set_num_threads(2);
+    omp_set_num_threads(1);
     double start =  omp_get_wtime();
     #pragma omp parallel
     {
-        printf("Liczba wątków: %d\n", omp_get_num_threads());
-        printf("Jestem wątek nr: %d\n", omp_get_thread_num());
         int i;
         #pragma omp for private (i)
         for(i = 1; i < MAX_SIZE/4; i++) {
