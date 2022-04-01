d3ee89896c4e6c36eef338057bed674d65393c80
Mateusz Obrzut <mobrzut@d17-430-pc-12.ipa.ki.agh.edu.pl>
2022-04-01 17:57:22 +0200

    l

--- lab3_OpenMP/5Program.c
+++ lab3_OpenMP/5Program.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 int main () {
-    const int MAX_SIZE = 100000;
+    const int MAX_SIZE = 1000000;
     srand(time(NULL));   // Initialization, should only be called once.
     // int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
     int arr[MAX_SIZE+10];
@@ -15,7 +15,7 @@ int main () {
         printf("Liczba wątków: %d\n", omp_get_num_threads());
         printf("Jestem wątek nr: %d\n", omp_get_thread_num());
         int i;
-        #pragma omp for 
+        #pragma omp for private (i)
         for(i = 1; i < MAX_SIZE/4; i++) {
             arr[(i*omp_get_thread_num())] = 1;
         }
