b7e272a943fe31086667de6f2a4c509db708161b
MateuszObrzut <mobrzut@qualtrics.com>
2022-04-29 01:16:50 +0200

    a

--- lab4_OpenMP/runner.sh
+++ lab4_OpenMP/runner.sh
@@ -10,7 +10,7 @@ mkdir -p temp
 function measure_alg() {
   results="temp/results.txt"
   echo "thread_number, task_array_size, bucket_size, repeat, fill_time, bucket_time, total_time, is_sorted" > "$results"
-  for i in {1..8}; do
+  for i in {1..20}; do
     ./bucketSort.x ${i} ${size} 4 50 | tee -a "$results"
   done
 }
